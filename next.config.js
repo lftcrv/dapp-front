e46782ad3bd3e7091359c6e1cf259e7f90522ee0
rmzlb <ramzi.laieb@gmail.com>
2025-01-29 10:42:43 +0100

    fix/connection-back
    fix/connection-back

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // ... existing config
+  env: {
+    PORT: 3000
+  },
+  // ... rest of existing config
+}
+
+module.exports = nextConfig 
