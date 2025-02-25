b94f5a9e8f4024a42ed8cd2db676d403b237448b
julienbrs <julien.bourseau@gmail.com>
2025-02-25 02:09:36 -0700

    hotfix

--- providers/StarknetProvider.tsx
+++ providers/StarknetProvider.tsx
@@ -1,22 +1,36 @@
 'use client';
+import { InjectedConnector } from 'starknetkit/injected';
+import { WebWalletConnector } from 'starknetkit/webwallet';
+import { mainnet, sepolia } from '@starknet-react/chains';
+import {
+  StarknetConfig,
+  jsonRpcProvider,
+  publicProvider,
+} from '@starknet-react/core';
+import { ReactNode } from 'react';
 
-import { InjectedConnector } from "starknetkit/injected";
-import { WebWalletConnector } from "starknetkit/webwallet";
-import { mainnet, sepolia } from "@starknet-react/chains";
-import { StarknetConfig, nethermindProvider, publicProvider } from "@starknet-react/core";
-import { ReactNode } from "react";
-
-export default function StarknetProvider({ children }: { children: ReactNode }) {
+export default function StarknetProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
   const defaultChain = process.env.NEXT_PUBLIC_DEFAULT_CHAIN;
-  const chains = defaultChain === sepolia.network ? [sepolia, mainnet] : [mainnet, sepolia];
-
+  const chains =
+    defaultChain === sepolia.network ? [sepolia, mainnet] : [mainnet, sepolia];
   const connectors = [
-    new InjectedConnector({ options: { id: "braavos", name: "Braavos" }}),
-    new InjectedConnector({ options: { id: "argentX", name: "Argent X" }}),
-    new WebWalletConnector({ url: "https://web.argent.xyz" }),
+    new InjectedConnector({ options: { id: 'braavos', name: 'Braavos' } }),
+    new InjectedConnector({ options: { id: 'argentX', name: 'Argent X' } }),
+    new WebWalletConnector({ url: 'https://web.argent.xyz' }),
   ];
 
-  const provider = publicProvider();
+  const PRIVATE_RPC_URL =
+    process.env.NEXT_PUBLIC_STARKNET_RPC_URL;
+
+  const provider = jsonRpcProvider({
+    rpc: (chain) => ({
+      nodeUrl: PRIVATE_RPC_URL,
+    }),
+  });
 
   return (
     <StarknetConfig
@@ -28,4 +42,4 @@ export default function StarknetProvider({ children }: { children: ReactNode }) 
       {children}
     </StarknetConfig>
   );
-} 
+}
