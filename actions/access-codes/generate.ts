2d493f4e6d655450e36adf9f6adb7c548a46b5cf
julienbrs <julien.bourseau@gmail.com>
2025-03-04 14:38:14 +0100

    fix loading secret in client server mode

--- actions/access-codes/generate.ts
+++ actions/access-codes/generate.ts
@@ -1,16 +1,6 @@
+'use server';
+
 import { AccessCode, GenerationParams } from '../../types/accessCode';
-
-const API_BASE_URL = '/api/access-code';
-const apiUrl = process.env.NEXT_PUBLIC_BACKEND_API_URL;
-const apiKey = process.env.API_KEY;
-
-if (!apiUrl || !apiKey) {
-  console.error('❌ Missing API configuration:', {
-    hasApiUrl: !!apiUrl,
-    hasApiKey: !!apiKey,
-  });
-  throw new Error('Missing API configuration');
-}
 
 /**
  * Generate one or more access codes with the provided parameters
@@ -20,6 +10,19 @@ if (!apiUrl || !apiKey) {
 export async function generateAccessCode(
   params: GenerationParams,
 ): Promise<AccessCode | AccessCode[]> {
+  const API_BASE_URL = '/api/access-code';
+  const apiUrl = process.env.NEXT_PUBLIC_BACKEND_API_URL;
+  const apiKey = process.env.API_KEY;
+
+  console.log('api url:', apiUrl);
+  console.log("api key:", apiKey)
+  if (!apiUrl || !apiKey) {
+    console.error('❌ Missing API configuration:', {
+      hasApiUrl: !!apiUrl,
+      hasApiKey: !!apiKey,
+    });
+    throw new Error('Missing API configuration');
+  }
   try {
     console.log(
       'Generating access code with params:',
