b48ada637615950ec382d2f3e29ac6ed6a9cf085
rmzlb <ramzi.laieb@gmail.com>
2025-03-25 17:38:11 +0100

    fix build

--- components/how-it-works/competition-section.tsx
+++ components/how-it-works/competition-section.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { memo, useState, Suspense } from 'react';
+import { useState, Suspense } from 'react';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
 import { TopAgents } from '@/components/top-agents';
